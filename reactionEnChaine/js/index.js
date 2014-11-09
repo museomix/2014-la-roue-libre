8722fa4be032c59c3651e8d9531c53bdb48e13b0
apiffaretti <adrien@avant-gout.com>
2014-11-09 10:30:54 +0100

    test include

--- reactionEnChaine/js/index.js
+++ reactionEnChaine/js/index.js
@@ -49,16 +49,5 @@ var app = {
         console.log('Received Event: ' + id);
 
 
-		cordova.plugins.barcodeScanner.scan(
-		      function (result) {
-		          alert("We got a barcode\n" +
-		                "Result: " + result.text + "\n" +
-		                "Format: " + result.format + "\n" +
-		                "Cancelled: " + result.cancelled);
-		      }, 
-		      function (error) {
-		          alert("Scanning failed: " + error);
-		      }
-		   );
     }
 };
